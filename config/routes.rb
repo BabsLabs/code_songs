0c3bf6d4f1382d27ffef53c5afe67d2507969981
Alice Post <alicep2322@gmail.com>
2019-12-18 15:36:28 -0700

    Add routes for welcome, connect to github, repos and logout
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+  get '/auth/github', as: 'github_connect'
+  get '/auth/github/callback', to: 'sessions#create'
+
+  get '/repos', to: 'repos#index'
+
+  delete '/logout', to: 'sessions#destroy'
 end
