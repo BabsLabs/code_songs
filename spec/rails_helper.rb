4e389bf84d6259ff628f104cd275ec3f07f5cef0
Alice Post <alicep2322@gmail.com>
2019-12-18 15:48:28 -0700

    Add shoulda matchers and simple cov to rails_helper
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
+
+SimpleCov.start "rails"
+
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -56,6 +60,12 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  Shoulda::Matchers.configure do |config|
+      config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
