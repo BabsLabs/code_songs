957171ebc17f32a09dea19bf61e6f2ba84c98298
Alice Post <alicep2322@gmail.com>
2019-12-18 15:33:10 -0700

    Add first sign-in with github test
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

---
+++ spec/features/github_connection_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'As a user, when I click "Sign in with GitHub"' do
+  it 'creates a new user and lets me login with GitHub' do
+    visit '/'
+
+    click_on "Sign in with GitHub"
+
+    expect(current_path).to eq('/repos')
+    expect(page).to have_content('Logout')
+
+    user = User.last
+    expect(user.github_token).to eq('mock_token')
+    expect(user.username).to eq('mockuser')
+
+    # users have attributes: username, github_token
+  end
+
+  it 'signs me in and does not create a new user if i have logged in before' do
+
+  end
+end
