d6a3858018545c2d637d35d2c8f87a1a26c1ba04
Alice Post <alicep2322@gmail.com>
2019-12-18 16:14:14 -0700

    Build out create session happy and sad path
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,29 @@
 class SessionsController < ApplicationController
   def create
+    # Find user by login => if it exists, make session, if no user found, make new user and log in
     # create a new user with github information
-    # set current_user to this user
-    # redirect to '/users/repos'
-    redirect_to '/repos'
+    user = User.create(user_params)
+    if user.save
+      flash[:success] = "You're logged in!"
+      session[:user_id] = user.id
+      redirect_to '/repos'
+    else
+      flash[:error] = "Failed to connect to GitHub"
+      redirect_to '/'
+    end
   end
 
   def destroy
-    
+
+  end
+
+
+  private
+
+  def user_params
+    {
+      login: request.env['omniauth.auth']['extra']['raw_info']['login'],
+      token: request.env['omniauth.auth']['credentials']['token']
+    }
   end
 end
