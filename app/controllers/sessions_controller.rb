d6cb0d0f1d2356e02ebcb138e05d12c6755fedee
Michael Cooper <macoop2363@gmail.com>
2019-12-19 13:15:32 -0700

    Remove unneeded conditional statement
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,9 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by(user_params)
-    if user
-      flash[:success] = "You're logged in!"
-      session[:user_id] = user.id
-      redirect_to '/repos'
-    else
-      flash[:error] = "Failed to connect to GitHub"
-      redirect_to '/'
-    end
+    flash[:success] = "You're logged in!"
+    session[:user_id] = user.id
+    redirect_to '/repos'
   end
 
   def destroy
