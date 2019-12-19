01ebd1ada217b7db444dacb12b850d216b32dfff
Michael Cooper <macoop2363@gmail.com>
2019-12-19 11:44:56 -0700

    Add test to ensure a previously registered user is not created again
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- spec/features/github_connection_spec.rb
+++ spec/features/github_connection_spec.rb
@@ -8,13 +8,24 @@ describe 'As a user, when I click "Sign in with GitHub"' do
 
     expect(current_path).to eq('/repos')
     expect(page).to have_content('Logout')
-
+    
     user = User.last
     expect(user.token).to eq('mock_token')
     expect(user.login).to eq('mockuser')
   end
-
+  
   it 'signs me in and does not create a new user if i have logged in before' do
+    user = create(:user, login: 'mockuser', token: 'mock_token')
+    user_count = User.count
+    
+    visit root_path
+    
+    click_on 'Sign in with GitHub'
 
+    expect(current_path).to eq('/repos')
+    expect(page).to have_content('Logout')
+    expect(page).to have_content("#{user.login}")
+
+    expect(User.count).to eq(user_count)
   end
 end
