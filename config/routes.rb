eeb5eaa889b1773049cc35f20580947d3e609e5f
Michael Cooper <macoop2363@gmail.com>
2019-12-19 12:12:57 -0700

    Add route for github handshake failure
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
+
   get '/auth/github', as: 'github_connect'
   get '/auth/github/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#failure'
 
   get '/repos', to: 'repos#index'
 
