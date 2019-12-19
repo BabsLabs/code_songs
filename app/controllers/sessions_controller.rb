4f3d202ad292ce8c975e0047c0b24d1be326b935
Michael Cooper <macoop2363@gmail.com>
2019-12-19 13:42:19 -0700

    Update user token when logging back in with Github
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,36 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(login: user_params[:login])
-    user = User.create(user_params) unless user
+    user = create_or_update_user
     flash[:success] = "You're logged in!"
     session[:user_id] = user.id
     redirect_to '/repos'
   end
-
+  
   def destroy
-
+    
   end
-
+  
   def failure
     flash[:error] = "Failed to connect to GitHub"
     redirect_to '/'
   end
-
+  
   private
-
+  
   def user_params
     {
       login: request.env['omniauth.auth']['extra']['raw_info']['login'],
       token: request.env['omniauth.auth']['credentials']['token']
     }
   end
-
   
+  def create_or_update_user
+    user = User.find_by(login: user_params[:login])
+    if user
+      user.update_attribute(:token, user_params[:token])
+    else    
+      user = User.create(user_params)
+    end
+    user
+  end
 end
