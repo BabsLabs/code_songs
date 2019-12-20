e1fc3b034312389961902be9ca372d7c5d54251e
Alice Post <alicep2322@gmail.com>
2019-12-19 19:18:34 -0700

    Start view and controller for searching a users repo - not finished
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- app/controllers/repos/search_controller.rb
+++ app/controllers/repos/search_controller.rb
@@ -1,4 +1,22 @@
 class Repos::SearchController < BaseController
-  def index
+  def create
+    @repos_found = find_github_repos(current_user.login, params[:search])
+    render 'repos/index'
+  end
+
+  def find_github_repos(user_login, search_term)
+    # Make real api call to give back a list of repos
+    # mock_repos_found = [
+    #   {
+    #     repo_name: 'monster_shop_1908',
+    #     owner: 'mockuser',
+    #     repo_id: 26877629,
+    #   },
+    #   {
+    #     repo_name: 'monster_shop_final',
+    #     owner: 'mockuser',
+    #     repo_id: 99999988,
+    #   },
+    # ]
   end
 end
