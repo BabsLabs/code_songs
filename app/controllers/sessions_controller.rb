b4e48ffc19420d07c63135540c25944c12140b8b
Alice Post <alicep2322@gmail.com>
2019-12-19 17:07:39 -0700

    Define sessions#destroy action
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,30 +5,31 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to '/repos'
   end
-  
+
   def destroy
-    
+    session.clear
+    redirect_to '/'
   end
-  
+
   def failure
     flash[:error] = "Failed to connect to GitHub"
     redirect_to '/'
   end
-  
+
   private
-  
+
   def user_params
     {
       login: request.env['omniauth.auth']['extra']['raw_info']['login'],
       token: request.env['omniauth.auth']['credentials']['token']
     }
   end
-  
+
   def create_or_update_user
     user = User.find_by(login: user_params[:login])
     if user
       user.update_attribute(:token, user_params[:token])
-    else    
+    else
       user = User.create(user_params)
     end
     user
