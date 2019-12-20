e51bd1830a1b1cc476ed74c9c096c1078594ef96
Alice Post <alicep2322@gmail.com>
2019-12-19 19:17:47 -0700

    Add and update repos search tests
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- spec/features/repos/repos_landing_spec.rb
+++ spec/features/repos/repos_landing_spec.rb
@@ -9,17 +9,8 @@ describe 'After a user signs in with GitHub, they see a repos page' do
     user = create(:user, login: 'mockuser', token: 'mock_token')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit '/repos'
-    
+
     expect(page).to have_css('#search')
     expect(page).to have_button('search')
   end
 end
-# User Story: As a user after I sign in with GitHub I see a landing page to select one of my github repos
-#
-# Epic: User Repo Search
-#
-# - [ ] There is a form to enter a repo name
-# - [ ] There is a button to submit your repo name
-# - [ ] Hits the GitHub Search API with the form params to search for a repo by name
-# - [ ] Sad path (No repo found)
-# - [ ] Tests written and passing
---
+++ spec/features/repos/repos_search_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'A signed in User can search for their repos' do
+  before(:all) do
+    OmniAuth.config.mock_auth[:github] = nil
+    stub_omniauth
+  end
+  
+  it 'can search for a list of repos' do
+    user = create(:user, login: 'mockuser', token: 'mock_token')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/repos'
+
+    fill_in 'search', with: 'monster_shop'
+    mock_repos_found = [
+      {
+        repo_name: 'monster_shop_1908',
+        owner: 'mockuser',
+        repo_id: 26877629,
+      },
+      {
+        repo_name: 'monster_shop_final',
+        owner: 'mockuser',
+        repo_id: 99999988,
+      },
+    ]
+    allow_any_instance_of(Repos::SearchController).to receive(:find_github_repos).and_return(mock_repos_found)
+    click_on 'search'
+
+    expect(current_path).to eq('/repos_search')
+
+    within '.found-repos' do
+      expect(page).to have_content('monster_shop_1908')
+      expect(page).to have_content('monster_shop_final')
+    end
+  end
+end
