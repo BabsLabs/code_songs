b5b403281eacf98c2e369a8b28efe7c616df2a7d
Alice Post <alicep2322@gmail.com>
2019-12-20 17:19:12 -0700

    Update feature test to use real GitHub repo api information
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- spec/features/repos/repos_search_spec.rb
+++ spec/features/repos/repos_search_spec.rb
@@ -5,26 +5,26 @@ describe 'A signed in User can search for their repos' do
     OmniAuth.config.mock_auth[:github] = nil
     stub_omniauth
   end
-  
-  it 'can search for a list of repos' do
-    user = create(:user, login: 'mockuser', token: 'mock_token')
+
+  it 'can search for a list of repos', :vcr do
+    user = create(:user, login: 'ap2322', token: ENV['GITHUB_TEST_TOKEN'])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit '/repos'
 
     fill_in 'search', with: 'monster_shop'
-    mock_repos_found = [
-      {
-        repo_name: 'monster_shop_1908',
-        owner: 'mockuser',
-        repo_id: 26877629,
-      },
-      {
-        repo_name: 'monster_shop_final',
-        owner: 'mockuser',
-        repo_id: 99999988,
-      },
-    ]
-    allow_any_instance_of(Repos::SearchController).to receive(:find_github_repos).and_return(mock_repos_found)
+    # mock_repos_found = [
+    #   {
+    #     repo_name: 'monster_shop_1908',
+    #     owner: 'mockuser',
+    #     repo_id: 26877629,
+    #   },
+    #   {
+    #     repo_name: 'monster_shop_final',
+    #     owner: 'mockuser',
+    #     repo_id: 99999988,
+    #   },
+    # ]
+    # allow_any_instance_of(Repos::SearchController).to receive(:find_github_repos).and_return(mock_repos_found)
     click_on 'search'
 
     expect(current_path).to eq('/repos_search')
