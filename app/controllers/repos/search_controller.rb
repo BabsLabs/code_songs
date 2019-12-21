69f4cfa0931c375b08e0501148502c217e6adbc3
Alice Post <alicep2322@gmail.com>
2019-12-20 17:24:05 -0700

    Refactor repos search controller to render locals with GitHubReposFacade
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- app/controllers/repos/search_controller.rb
+++ app/controllers/repos/search_controller.rb
@@ -1,22 +1,8 @@
 class Repos::SearchController < BaseController
-  def create
-    @repos_found = find_github_repos(current_user.login, params[:search])
-    render 'repos/index'
+  def index
+    render locals: {
+      repos_found: GitHubReposFacade.new(current_user.login, params[:search])
+    }, template: "repos/index"
   end
 
-  def find_github_repos(user_login, search_term)
-    # Make real api call to give back a list of repos
-    # mock_repos_found = [
-    #   {
-    #     repo_name: 'monster_shop_1908',
-    #     owner: 'mockuser',
-    #     repo_id: 26877629,
-    #   },
-    #   {
-    #     repo_name: 'monster_shop_final',
-    #     owner: 'mockuser',
-    #     repo_id: 99999988,
-    #   },
-    # ]
-  end
 end
