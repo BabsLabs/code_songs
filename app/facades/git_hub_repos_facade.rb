95e266ac7a694ff52b80ea4b315d3c2891c993a3
Alice Post <alicep2322@gmail.com>
2019-12-29 10:06:33 -0700

    Update repos facade and view to show message for no repos found/invalid search
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- app/facades/git_hub_repos_facade.rb
+++ app/facades/git_hub_repos_facade.rb
@@ -1,16 +1,24 @@
 class GitHubReposFacade
+  attr_reader :repos
+
   def initialize(login, search_term)
     @login = login
     @search_term = search_term
+    @repos = fetch_repos
   end
 
-  def repos
+  def fetch_repos
     service = GitHubService.new
     @repos_data ||= service.find_repos(login, search_term)
 
+    return [] unless @repos_data[:items]
     @repos_data[:items].map do |info|
       Repo.new(info)
     end
+  end
+
+  def bad_search
+    "No repo found, please search again"
   end
 
   private
