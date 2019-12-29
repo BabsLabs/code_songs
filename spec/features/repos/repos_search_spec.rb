cd96aa0cb88209e8edf94f349a86a5d138789f99
Alice Post <alicep2322@gmail.com>
2019-12-29 10:05:42 -0700

    Add sad path no repos found test and cassette
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- spec/features/repos/repos_search_spec.rb
+++ spec/features/repos/repos_search_spec.rb
@@ -12,19 +12,6 @@ describe 'A signed in User can search for their repos' do
     visit '/repos'
 
     fill_in 'search', with: 'monster_shop'
-    # mock_repos_found = [
-    #   {
-    #     repo_name: 'monster_shop_1908',
-    #     owner: 'mockuser',
-    #     repo_id: 26877629,
-    #   },
-    #   {
-    #     repo_name: 'monster_shop_final',
-    #     owner: 'mockuser',
-    #     repo_id: 99999988,
-    #   },
-    # ]
-    # allow_any_instance_of(Repos::SearchController).to receive(:find_github_repos).and_return(mock_repos_found)
     click_on 'search'
 
     expect(current_path).to eq('/repos_search')
@@ -33,5 +20,16 @@ describe 'A signed in User can search for their repos' do
       expect(page).to have_content('monster_shop_1908')
       expect(page).to have_content('monster_shop_final')
     end
+  end
+
+  it 'shows a flash message no repos found if no repos found', :vcr do
+    user = create(:user, login: 'ap2322', token: ENV['GITHUB_TEST_TOKEN'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/repos'
+
+    fill_in 'search', with: '$$'
+    click_on 'search'
+
+    expect(page).to have_content("No repo found, please search again")
   end
 end
