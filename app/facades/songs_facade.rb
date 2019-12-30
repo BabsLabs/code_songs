fabcfbb9997ab1fa498ce4bc04a27ef832f0933a
Alice Post <alicep2322@gmail.com>
2019-12-30 16:04:24 -0700

    Mock results in method until MatcherService is implemented
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>

--- app/facades/songs_facade.rb
+++ app/facades/songs_facade.rb
@@ -13,6 +13,14 @@ class SongsFacade
   def fetch_matching_songs
     # service = MatcherService.new
     # service.fetch_songs(@login, @token, @artist_id, @repo, @artist_name)
+
+    # Expect format of information returned from microservice below.
+    # Delete when MatcherService is implemented and running:
+    [ {title: 'Hound Dog', link: 'mock_link_hd'},
+      {title: "Can't Help Falling in Love", link: 'mock_link_love'},
+      {title: 'Suspicious Minds', link: 'mock_link_minds'},
+      {title: 'Jailhouse Rock', link: 'mock_link_jail'},
+      {title: 'Blue Hawaii', link: 'mock_link_hawaii'} ]
   end
 
   def songs_array
