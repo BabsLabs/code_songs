4803d8c4a1ea537f016884f257b2a14f44ec383f
Alice Post <alicep2322@gmail.com>
2020-01-02 10:22:15 -0700

    Basic api call to microservice testing 'hello world'
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

---
+++ app/services/songify_service.rb
@@ -0,0 +1,20 @@
+class SongifyService
+
+  def initialize(user, cart)
+    @token = user.token
+    @login = user.login
+    @repo = cart.repo
+    @artist_id = cart.artist_id
+  end
+
+  def songs
+    response = conn.get('/')
+    response.body
+  end
+
+  def conn
+    Faraday.new(url: 'https://code-songs-microservice.herokuapp.com') do |f|
+      f.adapter Faraday.default_adapter
+    end
+  end
+end
---
+++ spec/services/songify_service_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe SongifyService do
+  it 'connects to deployed heroku app', :vcr do
+    user = create(:user, login: 'ap2322', token: ENV['GITHUB_TEST_TOKEN'])
+    cart = SongifyCart.new({'repo' => 'battleship', 'artist_name' => 'Ed Sheeran', 'artist_id'=> '38620990'})
+
+    songify_service = SongifyService.new(user, cart)
+
+    expect(songify_service).to be_a(SongifyService)
+
+    expect(songify_service.songs).to eq 'Hello World!'
+  end
+end
