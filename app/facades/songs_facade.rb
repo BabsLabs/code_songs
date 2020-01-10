4c6e1806ecbdb592b66228e527079cdcb7e953c1
Alice Post <alicep2322@gmail.com>
2020-01-09 18:03:38 -0700

    Update songs_facade to make the SongifyService call to return matching songs
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- app/facades/songs_facade.rb
+++ app/facades/songs_facade.rb
@@ -2,25 +2,16 @@ class SongsFacade
   attr_reader :songs, :artist_name, :repo
 
   def initialize(current_user, songify_cart)
-    @login = current_user.login
-    @token = current_user.token
+    @user = current_user
+    @cart = songify_cart
     @artist_name = songify_cart.artist_name
-    @artist_id = songify_cart.artist_id
     @repo = songify_cart.repo
     @songs = songs_array
   end
 
   def fetch_matching_songs
-    # service = MatcherService.new
-    # service.fetch_songs(@login, @token, @artist_id, @repo, @artist_name)
-
-    # Expect format of information returned from microservice below.
-    # Delete when MatcherService is implemented and running:
-    [ {title: 'Hound Dog', link: 'mock_link_hd'},
-      {title: "Can't Help Falling in Love", link: 'mock_link_love'},
-      {title: 'Suspicious Minds', link: 'mock_link_minds'},
-      {title: 'Jailhouse Rock', link: 'mock_link_jail'},
-      {title: 'Blue Hawaii', link: 'mock_link_hawaii'} ]
+    service = SongifyService.new(@user, @cart)
+    service.match_songs
   end
 
   def songs_array
