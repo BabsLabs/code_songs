69a1857b369a296577c8cc1bc9f2d7f8774a60ec
Alice Post <alicep2322@gmail.com>
2020-01-09 20:24:17 -0700

    Add sad-path no tracks found from songify service to service and feature tests

--- spec/services/songify_service_spec.rb
+++ spec/services/songify_service_spec.rb
@@ -27,4 +27,13 @@ describe SongifyService do
     expect(songs_collection[:songs].first).to have_key :title
     expect(songs_collection[:songs].first).to have_key :link
   end
+
+  it 'handles json for codesongs_matcher endpoint with no tracks', :vcr do
+    user = create(:user, login: 'ap2322', token: ENV['GITHUB_TEST_TOKEN'])
+    cart = SongifyCart.new({'repo' => 'battleship', 'artist_name' => 'Invalid', 'artist_id'=> '38020124'})
+
+    songify_service = SongifyService.new(user, cart)
+    songs_collection = songify_service.match_songs
+    expect(songs_collection).to eq [{:error=>"Artist has no tracks."}]
+  end
 end
